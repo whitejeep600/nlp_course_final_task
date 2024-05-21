37fcb879761f37c004d685bc1f08b6c84ef93dbd
neil870925ab <52836478+neil870925ab@users.noreply.github.com>
2024-05-21 13:37:49 +0800

    adding some codes to deal with test data
    There are 2 important things:
    1. the format of the training data of task1 is different from the one of task2
    2. adding or changing some codes to deal with the test data of task1 through "test_mode"

--- src/datasets/argument_relation_detection_dataset.py
+++ src/datasets/argument_relation_detection_dataset.py
@@ -13,28 +13,34 @@ class ArgumentRelationDetectionDataset(Dataset):
         self,
         json_path: Path,
         tokenizer: PreTrainedTokenizer,
+        test_mode: bool = False,
     ):
         with open(json_path, "r") as f:
             data = json.load(f)
-        self.texts: list[tuple[str, str]] = [(sample[0], sample[1]) for sample in data]
-        self.labels: list[int] = [sample[2] for sample in data]
+        self.texts: list[tuple[str, str]] = [(sample[1], sample[2]) for sample in data]
+        self.labels: list[int] = [sample[3] for sample in data] if not test_mode else None
         self.tokenizer = tokenizer
+        self.test_mode = test_mode
 
     def __len__(self):
-        return len(self.labels)
+        return len(self.texts)
 
     def __getitem__(self, i):
         texts = self.texts[i]
-        label = self.labels[i]
+        label = self.labels[i] if self.labels is not None else None
         tokenized_text = self.tokenizer(
             *texts,
             return_tensors="pt",
             padding="max_length",
             truncation=True,
         )
-        return {
+
+        data_dict = {
             INPUT_IDS: tokenized_text[INPUT_IDS].flatten(),
             ATTENTION_MASK: tokenized_text[ATTENTION_MASK].flatten(),
             TOKEN_TYPE_IDS: tokenized_text[TOKEN_TYPE_IDS].flatten(),
-            LABEL: torch.tensor(label),
         }
+        if not self.test_mode:
+            data_dict[LABEL] = torch.tensor(label)
+        
+        return data_dict
